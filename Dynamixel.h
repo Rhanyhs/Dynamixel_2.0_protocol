5d094d11e8c920865016ab0a236359ac243c4b99
Rhanyhs <alperen.eroll01@gmail.com>
2024-05-13 13:01:55 +0300

    Crc changed

--- Dynamixel.h
+++ Dynamixel.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdio.h>
 #define DLX_PACKET_BUFF_MAX	100
 typedef uint8_t uint8;
 typedef uint16_t uint16;
@@ -18,8 +19,8 @@ typedef struct dxl_packet
 	uint8 lenght_h;  //tek bir uint16 tipinde lenght te olabilir kararsýz kaldým
 	uint8 inst;
 	uint8 err;
-	uint16 param_len;
-	uint8* param;
+	
+	uint16 crc;
 
 }dxl_packet;
 
@@ -27,8 +28,13 @@ typedef struct dxl
 {
 	uint8 bIsOpen;
 	uint8 state;
-	uint8 is_status_packet;
+	uint16 packet_length;
+	uint8* param;
+	size_t index;
+	size_t param_index;
+	uint8 param_len;
 	dxl_packet packet;
+
 
 	uint8 packet_buff[DLX_PACKET_BUFF_MAX];
 
@@ -36,7 +42,7 @@ typedef struct dxl
 
 
 
-enum Packet_Bytes
+enum Packet_States
 {
 	DXL_STATUS_HEADER1 = 0,
 	DXL_STATUS_HEADER2 = 1,
@@ -48,14 +54,29 @@ enum Packet_Bytes
 	DXL_STATUS_Inst = 7,
 	DXL_STATUS_Err = 8,
 	DXL_STATUS_Param = 9,
+	DXL_STATUS_CRC_L = 10,
+	DXL_STATUS_CRC_H = 11,
 	
+};
+
+enum Error
+{
+	No_Error = 0x00,
+	Result_Fail = 0x01,
+	Instruction_Error = 0x02,
+	CRC_Error = 0x03,
+	Data_Range_Error = 0x04,
+	Data_Length_Error = 0x05,
+	Data_Limit_Error = 0x06,
+	Access_Error = 0x07,
+
 };
 
 
 
 
 
-uint8 Send_Instruction(dxl* p_dxl, uint8 id, uint8 inst, uint8* p_param, uint16* param_len);
-uint8 Receive_Status_Packet(dxl* p_dxl,uint8 rxbuff[]);
-
+//uint8 Send_Instruction(dxl* p_dxl, uint8 id, uint8 inst, uint8* p_param, uint16* param_len);
+extern uint8 Receive_Status_Packet(dxl* p_dxl,uint8 rxbuff[], size_t rxbuff_size);
+extern unsigned short update_crc(unsigned short crc_accum, unsigned char* data_blk_ptr, unsigned short data_blk_size);
 #endif
--- Dynamixel_2.0_protocol.c
+++ Dynamixel_2.0_protocol.c
@@ -1,11 +1,66 @@
 ﻿#include "Dynamixel.h"
 
+
+unsigned short update_crc(unsigned short crc_accum, unsigned char* data_blk_ptr, unsigned short data_blk_size)
+{
+	unsigned short i, j;
+	unsigned short crc_table[256] = {
+		0x0000, 0x8005, 0x800F, 0x000A, 0x801B, 0x001E, 0x0014, 0x8011,
+		0x8033, 0x0036, 0x003C, 0x8039, 0x0028, 0x802D, 0x8027, 0x0022,
+		0x8063, 0x0066, 0x006C, 0x8069, 0x0078, 0x807D, 0x8077, 0x0072,
+		0x0050, 0x8055, 0x805F, 0x005A, 0x804B, 0x004E, 0x0044, 0x8041,
+		0x80C3, 0x00C6, 0x00CC, 0x80C9, 0x00D8, 0x80DD, 0x80D7, 0x00D2,
+		0x00F0, 0x80F5, 0x80FF, 0x00FA, 0x80EB, 0x00EE, 0x00E4, 0x80E1,
+		0x00A0, 0x80A5, 0x80AF, 0x00AA, 0x80BB, 0x00BE, 0x00B4, 0x80B1,
+		0x8093, 0x0096, 0x009C, 0x8099, 0x0088, 0x808D, 0x8087, 0x0082,
+		0x8183, 0x0186, 0x018C, 0x8189, 0x0198, 0x819D, 0x8197, 0x0192,
+		0x01B0, 0x81B5, 0x81BF, 0x01BA, 0x81AB, 0x01AE, 0x01A4, 0x81A1,
+		0x01E0, 0x81E5, 0x81EF, 0x01EA, 0x81FB, 0x01FE, 0x01F4, 0x81F1,
+		0x81D3, 0x01D6, 0x01DC, 0x81D9, 0x01C8, 0x81CD, 0x81C7, 0x01C2,
+		0x0140, 0x8145, 0x814F, 0x014A, 0x815B, 0x015E, 0x0154, 0x8151,
+		0x8173, 0x0176, 0x017C, 0x8179, 0x0168, 0x816D, 0x8167, 0x0162,
+		0x8123, 0x0126, 0x012C, 0x8129, 0x0138, 0x813D, 0x8137, 0x0132,
+		0x0110, 0x8115, 0x811F, 0x011A, 0x810B, 0x010E, 0x0104, 0x8101,
+		0x8303, 0x0306, 0x030C, 0x8309, 0x0318, 0x831D, 0x8317, 0x0312,
+		0x0330, 0x8335, 0x833F, 0x033A, 0x832B, 0x032E, 0x0324, 0x8321,
+		0x0360, 0x8365, 0x836F, 0x036A, 0x837B, 0x037E, 0x0374, 0x8371,
+		0x8353, 0x0356, 0x035C, 0x8359, 0x0348, 0x834D, 0x8347, 0x0342,
+		0x03C0, 0x83C5, 0x83CF, 0x03CA, 0x83DB, 0x03DE, 0x03D4, 0x83D1,
+		0x83F3, 0x03F6, 0x03FC, 0x83F9, 0x03E8, 0x83ED, 0x83E7, 0x03E2,
+		0x83A3, 0x03A6, 0x03AC, 0x83A9, 0x03B8, 0x83BD, 0x83B7, 0x03B2,
+		0x0390, 0x8395, 0x839F, 0x039A, 0x838B, 0x038E, 0x0384, 0x8381,
+		0x0280, 0x8285, 0x828F, 0x028A, 0x829B, 0x029E, 0x0294, 0x8291,
+		0x82B3, 0x02B6, 0x02BC, 0x82B9, 0x02A8, 0x82AD, 0x82A7, 0x02A2,
+		0x82E3, 0x02E6, 0x02EC, 0x82E9, 0x02F8, 0x82FD, 0x82F7, 0x02F2,
+		0x02D0, 0x82D5, 0x82DF, 0x02DA, 0x82CB, 0x02CE, 0x02C4, 0x82C1,
+		0x8243, 0x0246, 0x024C, 0x8249, 0x0258, 0x825D, 0x8257, 0x0252,
+		0x0270, 0x8275, 0x827F, 0x027A, 0x826B, 0x026E, 0x0264, 0x8261,
+		0x0220, 0x8225, 0x822F, 0x022A, 0x823B, 0x023E, 0x0234, 0x8231,
+		0x8213, 0x0216, 0x021C, 0x8219, 0x0208, 0x820D, 0x8207, 0x0202
+	};
+
+	for (j = 0; j < data_blk_size; j++)
+	{
+		i = ((unsigned short)(crc_accum >> 8) ^ data_blk_ptr[j]) & 0xFF;
+		crc_accum = (crc_accum << 8) ^ crc_table[i];
+	}
+
+	return crc_accum;
+}
+
+
 uint8 Receive_Status_Packet(dxl* p_dxl, uint8 rxbuff[], size_t rxbuff_size)
 {
-	uint16 param_lenght = 0;
-	uint16 packet_length = 0;
-
-	p_dxl->state = NULL;
+	
+	uint16 temp_param_len = 0;
+	uint8 retval = 0;
+	uint8 param_index = 0;
+	int k = 0;
+	unsigned short calc_crc;
+	p_dxl->state = DXL_STATUS_HEADER1;
+	p_dxl->index = 0;
+	p_dxl->param_index = 0;
+	p_dxl->packet_length = 0;
 	for (size_t i = 0; i < rxbuff_size; ++i)
 	{
 		switch (p_dxl->state)
@@ -13,78 +68,204 @@ uint8 Receive_Status_Packet(dxl* p_dxl, uint8 rxbuff[], size_t rxbuff_size)
 			case DXL_STATUS_HEADER1:
 				if (rxbuff[i] == 0xFF)
 				{
-					p_dxl->packet_buff[DXL_STATUS_HEADER1] = rxbuff[i];
+					p_dxl->packet_buff[p_dxl->index] = rxbuff[i];
 					p_dxl->state = DXL_STATUS_HEADER2;
 					p_dxl->packet.header1 = rxbuff[i];
+					++p_dxl->index;
 				}
 				break;
 
 			case DXL_STATUS_HEADER2:
 				if (rxbuff[i] == 0xFF)
 				{
-					p_dxl->packet_buff[DXL_STATUS_HEADER2] = rxbuff[i];
+					p_dxl->packet_buff[p_dxl->index] = rxbuff[i];
 					p_dxl->state = DXL_STATUS_HEADER3;
 					p_dxl->packet.header2 = rxbuff[i];
+					++p_dxl->index;
 				}
 				else
 				{
 					p_dxl->state = DXL_STATUS_HEADER1;
+					p_dxl->index = 0;
 				}
 				break;
 
 			case DXL_STATUS_HEADER3:
-				if (rxbuff[i] == 0xFF)
+				if (rxbuff[i] == 0xFD)
 				{
-					p_dxl->packet_buff[DXL_STATUS_HEADER3] = rxbuff[i];
+					p_dxl->packet_buff[p_dxl->index] = rxbuff[i];
 					p_dxl->state = DXL_STATUS_RESERVED;
 					p_dxl->packet.header3 = rxbuff[i];
+					++p_dxl->index;
 				}
 				else
 				{
 					p_dxl->state = DXL_STATUS_HEADER1;
+					p_dxl->index = 0;
 				}
 				break;
 
 			case DXL_STATUS_RESERVED:
 				if (rxbuff[i] == 0x00)
 				{
-					p_dxl->packet_buff[DXL_STATUS_RESERVED] = rxbuff[i];
+					p_dxl->packet_buff[p_dxl->index] = rxbuff[i];
 					p_dxl->state = DXL_STATUS_ID;
 					p_dxl->packet.reserved = rxbuff[i];
+					++p_dxl->index;
 				}
 				break;
 
 			case DXL_STATUS_ID:
-				p_dxl->packet_buff[DXL_STATUS_ID] = rxbuff[i];
+				p_dxl->packet_buff[p_dxl->index] = rxbuff[i];
 				p_dxl->state = DXL_STATUS_Len1;
 				p_dxl->packet.id = rxbuff[i];
+				++p_dxl->index;
 				break;
 
 			case DXL_STATUS_Len1:
-				p_dxl->packet_buff[DXL_STATUS_Len1] = rxbuff[i];
+				p_dxl->packet_buff[p_dxl->index] = rxbuff[i];
 				p_dxl->state = DXL_STATUS_Len2;
 				p_dxl->packet.length_l = rxbuff[i];
+				++p_dxl->index;
 				break;
 
 			case DXL_STATUS_Len2:
-				p_dxl->packet_buff[DXL_STATUS_Len2] = rxbuff[i];
+				p_dxl->packet_buff[p_dxl->index] = rxbuff[i];
 				p_dxl->state = DXL_STATUS_Inst;
 				p_dxl->packet.lenght_h = rxbuff[i];
+				++p_dxl->index;
 				break;
 
 			case DXL_STATUS_Inst:
-				packet_length = (uint16)p_dxl->packet_buff[DXL_STATUS_Len1];
-				packet_length = (uint16)p_dxl->packet_buff[DXL_STATUS_Len1] << 8;
-				if (p_dxl->packet_buff[DXL_STATUS_Inst] == 0x55)
+				p_dxl->packet_length = (uint16)p_dxl->packet_buff[DXL_STATUS_Len1];
+				p_dxl->packet_length |= (uint16)p_dxl->packet_buff[DXL_STATUS_Len2] << 8;
+				if (rxbuff[i] == 0x55)
 				{
-					p_dxl->packet_buff[DXL_STATUS_Inst] = rxbuff[i];
-					param_lenght = packet_length - 4;
+					p_dxl->packet_buff[p_dxl->index] = rxbuff[i];
+					p_dxl->packet.inst = rxbuff[i];
+					p_dxl->param_len = p_dxl->packet_length - 4;
+					++p_dxl->index;
+					//p_dxl->packet.param = p_dxl->packet_buff + p_dxl->index;
+					p_dxl->state = DXL_STATUS_Err;
 				}
+				else
+				{
+					p_dxl->state = DXL_STATUS_HEADER1;
+					p_dxl->index = 0;
+				}
+				break;
+
+			case DXL_STATUS_Err:
+				p_dxl->packet_buff[p_dxl->index] = rxbuff[i];
+				p_dxl->packet.err = rxbuff[i];
+				switch (p_dxl->packet_buff[DXL_STATUS_Err])
+				{
+				case No_Error:
+					printf("There is no error in received message\n");
+					break;
+
+				case Result_Fail:
+					printf("Result_Fail Error\n");
+					break;
+
+				case Instruction_Error:
+					printf("Instruction_Error\n");
+					break;
+
+				case CRC_Error:
+					printf("CRC_Error\n");
+					break;
+
+				case Data_Range_Error:
+					printf("Data_Range_Error\n");
+					break;
+
+				case Data_Length_Error:
+					printf("Data_Length_Error\n");
+					break;
+
+				case Data_Limit_Error:
+					printf("Data_Limit_Error\n");
+					break;
+
+				case Access_Error:
+					printf("Access_Error\n");
+					break;
+					
+				}
+				p_dxl->state = DXL_STATUS_Param;
+				++p_dxl->index;
+				break;
+
+			case DXL_STATUS_Param:
+				//temp_param_len = p_dxl->param_len;
+				
+				if (k < p_dxl->param_len)
+				{
+					p_dxl->packet_buff[p_dxl->index++] = rxbuff[i];
+					++k;
+				}
+				else
+				{
+					p_dxl->param = &(p_dxl->packet_buff[DXL_STATUS_Param]);
+					p_dxl->state = DXL_STATUS_CRC_L;
+					--i;
+				}
+				
+				break;
+
+			case DXL_STATUS_CRC_L:
+				p_dxl->packet_buff[p_dxl->index++] = rxbuff[i];
+				p_dxl->packet.crc = (uint16)rxbuff[i];
+				p_dxl->state = DXL_STATUS_CRC_H;
+				break;
+
+			case DXL_STATUS_CRC_H:
+				p_dxl->packet_buff[p_dxl->index++] = rxbuff[i];
+				p_dxl->packet.crc |= (uint16)rxbuff[i] << 8;
+				calc_crc = update_crc(0, p_dxl->packet_buff, p_dxl->packet_length + 5);
+				if (calc_crc == p_dxl->packet.crc)
+				{
+					printf("CRC dogru\n");
+				}
+				else
+					printf("basarisiz\n");
+				break;
+				
 
 
 
 
-		}	
+		}
+		
 				
 	}
+	return retval;
+}
+int main()
+{
+	uint16 packet_length = 0;
+	uint8 arr[14] = { 0xFF,0xFF,0xFD,0x00,0x01,0x07,0x00,0x55,0x00,0x06,0x04,0x26,0x65,0x5D };
+	dxl example;
+	uint8 arr2[15] = { 0xFF,0xFF,0xFD,0x00,0x02,0x08,0x00,0x55,0x00,0x1F,0x08,0x00,0x00,0xBA,0xBE };
+	//uint16 crc = (uint16)arr[12];
+	//crc |= (uint16)arr[13] << 8;
+	Receive_Status_Packet(&example, arr2,15);
+	//printf("%x\n", crc);
+	//printf("%x", update_crc(0, arr, 12));
+	printf("header1 = %x\n", example.packet.header1);
+	printf("header2 = %x\n", example.packet.header2);
+	printf("header3 = %x\n", example.packet.header3);
+	printf("reserved = %x\n", example.packet.reserved);
+	printf("id = %x\n", example.packet.id);
+	printf("lengthl = %x\n", example.packet.length_l);
+	printf("lengthh = %x\n", example.packet.lenght_h);
+	printf("inst = %x\n", example.packet.inst);
+	printf("err = %x\n", example.packet.err);
+	for (size_t i = 0; i < example.param_len; ++i)
+	{
+		printf("%d param = %x\n", i + 1, example.param[i]);
+	}
+	printf("calc crc = %x\n", example.packet.crc);
+
 }
